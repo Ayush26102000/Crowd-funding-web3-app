b6d32904ea8441253ad81358093ea1fcf6a07f05
Ayush Kamble <aykkamble098@gmail.com>
2023-01-19 22:59:07 +0530

    Basic Structure completed

--- client/crowdfunding/src/App.jsx
+++ client/crowdfunding/src/App.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { Navbar, Sidebar } from './components';
+
+
+import { CampaignDetails, CreateCampaign, Home, Profile } from './pages';
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
+      <div className="sm:flex hidden mr-10 relative">
+        <Sidebar />
+      </div>
+
+      <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
+        <Navbar /></div>
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+
+      </BrowserRouter>
+
+
+    </div>
   )
 }
 
