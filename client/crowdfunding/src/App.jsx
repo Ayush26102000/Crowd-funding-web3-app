ce1e76396858bda68b6fb13054efffe0739bdf0a
Ayush Kamble <aykkamble098@gmail.com>
2023-02-05 20:43:00 +0530

    sidebar completed

--- client/crowdfunding/src/App.jsx
+++ client/crowdfunding/src/App.jsx
@@ -8,24 +8,18 @@ import { CampaignDetails, CreateCampaign, Home, Profile } from './pages';
 
 const App = () => {
   return (
-    <div className="relative sm:-8 p-4 bg-[#FFFFFF] min-h-screen flex flex-row">
-      <div className="sm:flex hidden mr-10 relative">
-        <Sidebar />
-      </div>
-
-      <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
-        <Navbar /></div>
-
-      <BrowserRouter>
-      
+    <BrowserRouter>
+      <div className="relative sm:-8 p-4 bg-[#FFFFFF] min-h-screen flex flex-row">
+        <div className="sm:flex hidden mr-10 relative">
+          <Sidebar />
+        </div>
+        <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
+          <Navbar /></div>
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
-
-      </BrowserRouter>
-
-
-    </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
